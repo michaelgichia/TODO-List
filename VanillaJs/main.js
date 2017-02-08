d7c3f5a8e524ad40180cdc7fe1b4314d5c1dbf70
Michael Gichia <mqyynm@gmail.com>
2017-02-08 18:51:55 +0300

    :art: Refactoring and cleanup.

--- VanillaJs/main.js
+++ VanillaJs/main.js
@@ -1,189 +1,174 @@
-// Todo store
-var todosArray = []
-// Event Listener
-var todos = document.querySelector('.todos');
-var todosInput = document.querySelector('.todo-input');
-var actions = document.querySelector('.actions');
+// Stores all to do list
+const todosArray = [];
+// eslint-disable-next-line
+const todos = document.querySelector('.todos');
+// eslint-disable-next-line
+const todosInput = document.querySelector('.todo-input');
+// eslint-disable-next-line
+const actions = document.querySelector('.actions');
 
-todosInput.addEventListener('keypress', addTodo)
-todos.addEventListener('click', handlers)
-todos.addEventListener('dblclick', editTodo)
-actions.addEventListener('click', actionHandler)
+// Render todos to the browser
+function renderTodos(elTodo, array) {
+  todos.innerHTML = array.map((todo, index) =>
+    `
+      <li>
+        <input
+          type="checkbox"
+          class="checkbox"
+          id="${index}"
+          ${todo.completed ? 'checked' : ''} />
+        <label
+          class="label${index}"
+          id="${index}">
+          ${todo.todoContent}
+        </label>
+        <button class="transparent" id="${index}">&times;</button>
+      </li>
+    `).join('');
+}
 
 /**
  * Add an item to the todosArray and display in on the
  * the browser.
- * 
- * @param {boolean} completed Differentiate completed or 
- *                  active todos.
+ *
 */
-function addTodo (e) {
-  var key = e.which || e.charCode
-  // Select input tag value with class`todo-input`
-  var todoContent = document.querySelector('.todo-input');
-  // Prevent page reload on empty values
+function addTodo(e) {
+  const key = e.which || e.charCode;
+  // eslint-disable-next-line
+  const todoContent = document.querySelector('.todo-input');
   if (key === 13 && todoContent.value.length < 2) {
     e.preventDefault();
     return false;
   }
-  // Check if the key is charCode 13/entering key
   if (key === 13 && todoContent.value.length > 2) {
     e.preventDefault();
-    todosArray.push({todoContent: todoContent.value, completed: false});
-    // Call renderTodos with the new todo items
-    renderTodos(todos, todosArray)
-    // Initialize the input to blank
+    todosArray.push({ todoContent: todoContent.value, completed: false });
+    renderTodos(todos, todosArray);
     todoContent.value = '';
-    // Avoid page reload
     return false;
   }
-}
-
-/**
- * Check if we have todos and display them on the browser.
-*/
-function renderTodos (todos, todosArray) {
-  todos.innerHTML = todosArray.map((todo, index) => {
-    return `
-      <li>
-        <input type="checkbox" class="checkbox" id="${index}" ${todo.completed ? "checked" : ""} />
-        <label class="label${index}" id="${index}">${todo.todoContent}</label>
-        <button class="transparent" id="${index}">&times;</button>
-      </li>
-    `
-  }).join('');
+  return renderTodos(todos, todosArray);
 }
 
 /**
  * Edit todo on double click on todoContent.
- * We create and input html element to edit 
- * todoContent and push it on the same array position.
- * Call renderTodo function to display new todoContent
+ * Creates a textarea element so as to edit todoContent
+ * and push it on the same position in array.
  *
- * @param {integer} position Position of a todo on the array.
- * @param {string} todoContent Todo content.
 */
-function editTodo (e) {  
-  // When a todo is created, it is set id attribute
+function editTodo(e) {
   // with the value of its index(its position on the todosArray
-  // that stores all todos).
-  var position = parseInt(e.target.id);
+  const position = parseInt(e.target.id, 10);
   // Todos has an input and label elements.
-  // label id are created by concatinating position
+  // label id are created by concatinating `position`
   // and string `label`.
-  // We can access the label dynamically because position
-  // are unique, index of todos object in the todosArray.
-  var labelId = `.label${position}`
-  var oldElement = document.querySelector(labelId)
-  var parentNode = oldElement.parentNode
+  const labelId = `.label${position}`;
+  // eslint-disable-next-line
+  const oldElement = document.querySelector(labelId);
+  const parentNode = oldElement.parentNode;
   // Create an input element with the attributes below
-  var inputElement = document.createElement('textarea');
+  // eslint-disable-next-line
+  const inputElement = document.createElement('textarea');
   inputElement.setAttribute('class', 'change-todo materialize-textarea');
   inputElement.setAttribute('id', `textarea${position}`);
-  // Replace the child node label with input so 
+  // Replace the child node label with input so
   // that we can edit the todoContent.
-  var todoContent = todosArray[position];
+  const todoContent = todosArray[position];
   inputElement.value = todoContent.todoContent;
-  parentNode.replaceChild(inputElement, oldElement)
-  // Get the class of newly create input element.
-  var changeTodoInput = document.querySelector('.change-todo');
-  // At this point, We are editing the todoContent.
-  // The below function is similar as to add a new todo.
-  changeTodoInput.addEventListener('keypress', function (e) {
-    var key = e.which || e.charCode
-    var index = position
-    var todoContent = e.target.value
-    var todoObject = {todoContent: todoContent, completed: false}
-    // Make sure is charCode 13
-    if (key === 13 && e.target.value.length > 2) {
-      todosArray.splice(index, 1, todoObject)
-      renderTodos(todos, todosArray)
-    }  
-  })
-}
-
-function actionHandler (e) {
-  allTodo(e);
-  toggleAll(e);
-  activeTodo(e);
-  completedTodo(e);
+  parentNode.replaceChild(inputElement, oldElement);
+  // eslint-disable-next-line
+  const changeTodoInput = document.querySelector('.change-todo');
+  changeTodoInput.addEventListener('keypress', (event) => {
+    const key = event.which || event.charCode;
+    const index = position;
+    const newTodoContent = event.target.value;
+    const todoObject = { todoContent: newTodoContent, completed: false };
+    if (key === 13 && event.target.value.length > 2) {
+      todosArray.splice(index, 1, todoObject);
+      renderTodos(todos, todosArray);
+    }
+  });
 }
 
 /**
- * Toggle all todos as completed or active.
+ * Toggle all todos from completed to active and vice versa.
 */
-function toggleAll (e) {
+function toggleAll(e) {
   if (!e.target.matches('#toggle-all')) return;
-  var noOfActiveTodos = (todosArray.filter(todo => !!todo.completed)).length;
-  var noOfCompletedTodos = (todosArray.filter(todo => !todo.completed)).length;
-
+  const noOfActiveTodos = (todosArray.filter(todo => !!todo.completed)).length;
+  const noOfCompletedTodos = (todosArray.filter(todo => !todo.completed)).length;
   if (noOfActiveTodos < todosArray.length) {
-    todosArray.map(todo => todo.completed = true);
-
-  } else if(noOfCompletedTodos < todosArray.length) {
-    todosArray.map(todo => todo.completed = false);
-
+    todosArray.map(todo => (todo.completed = true));
+  } else if (noOfCompletedTodos < todosArray.length) {
+    todosArray.map(todo => (todo.completed = false));
   } else {
-    todosArray.map(todo => todo.completed = !todo.completed);
-
+    todosArray.map(todo => (todo.completed = !todo.completed));
   }
-  renderTodos(todos, todosArray)
+  renderTodos(todos, todosArray);
 }
 
 /**
- * Show active todos.
+ * Display all active todos.
 */
-function activeTodo (e) {
+function activeTodo(e) {
   if (!e.target.matches('#active-todo')) return;
-  var activeTodos = todosArray.filter( todo => !todo.completed)
-  renderTodos(todos, activeTodos)
+  const activeTodos = todosArray.filter(todo => !todo.completed);
+  renderTodos(todos, activeTodos);
 }
 
 /**
- * Show completed todos.
+ * Display all completed todos.
 */
-function completedTodo (e) {
+function completedTodo(e) {
   if (!e.target.matches('#complete-todo')) return;
-  var completedTodos = todosArray.filter( todo => !!todo.completed)
-  renderTodos(todos, completedTodos)
+  const completedTodos = todosArray.filter(todo => !!todo.completed);
+  renderTodos(todos, completedTodos);
 }
 
-
 /**
- * Show all todos.
+ * Display both completed and active todos.
 */
-function allTodo (e) {
+function allTodo(e) {
   if (!e.target.matches('#all-todo')) return;
-  renderTodos(todos, todosArray)
-}
-
-function handlers (e) {
-  toggleTodo(e);
-  deleteTodo(e);
+  renderTodos(todos, todosArray);
 }
 
 /**
- * Interchange a todo as completed or active.
- *
- * @param {integer} position Position of a todo on the array.
+ * Toggle a todo from completed to active and vice versa.
 */
-function toggleTodo (e) {
-  var index = parseInt(e.target.id)
+function toggleTodo(e) {
+  const index = parseInt(e.target.id, 10);
   if (!e.target.matches('.checkbox')) return;
-  var todo = todosArray[index];
+  const todo = todosArray[index];
   todo.completed = !(todo.completed);
-  todosArray.splice(index, 1, todo)
-  renderTodos(todos, todosArray); 
+  todosArray.splice(index, 1, todo);
+  renderTodos(todos, todosArray);
 }
 
 /**
  * Delete todo when delete button is clicked.
  *
 */
-function deleteTodo (e) {
-  var index = parseInt(e.target.id)
+function deleteTodo(e) {
+  const index = parseInt(e.target.id, 10);
   if (!e.target.matches('button')) return;
-  todosArray.splice(index, 1)
-  renderTodos(todos, todosArray)
+  todosArray.splice(index, 1);
+  renderTodos(todos, todosArray);
 }
 
+function handlers(e) {
+  toggleTodo(e);
+  deleteTodo(e);
+}
+
+function actionHandler(e) {
+  allTodo(e);
+  toggleAll(e);
+  activeTodo(e);
+  completedTodo(e);
+}
+
+todosInput.addEventListener('keypress', addTodo);
+todos.addEventListener('click', handlers);
+todos.addEventListener('dblclick', editTodo);
+actions.addEventListener('click', actionHandler);
