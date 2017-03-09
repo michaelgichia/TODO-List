e1a5deea9c87d1a3bce726298193f35086873389
michaelgichia <mqyynm@gmail.com>
2017-03-09 12:22:34 +0300

    :zap: Material-ui.

--- ReduxReactToDo/app/containers/App/index.js
+++ ReduxReactToDo/app/containers/App/index.js
@@ -12,6 +12,8 @@
  */
 
 import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
@@ -21,9 +23,12 @@ export default class App extends React.PureComponent { // eslint-disable-line re
 
   render() {
     return (
-      <div>
-        {React.Children.toArray(this.props.children)}
-      </div>
+      <MuiThemeProvider>
+        <div>
+          {React.Children.toArray(this.props.children)}
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
+
