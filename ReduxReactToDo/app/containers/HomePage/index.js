862c3bb58f8e93bc3360e765cbbcfbbad894fbbe
michaelgichia <mqyynm@gmail.com>
2017-03-09 12:23:45 +0300

    :zap: Homepage Styling.

--- ReduxReactToDo/app/containers/HomePage/index.js
+++ ReduxReactToDo/app/containers/HomePage/index.js
@@ -1,24 +1,25 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
-
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import NavBar from 'components/NavBar';
+import TodoInputText from 'components/TodoInputText';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+      	<header>
+					<NavBar />
+      	</header>
+				<Grid fluid>
+				  <Row>
+				    <Col lgOffset={4} lg={8} xs={10} md={8}>
+							<TodoInputText />
+				    </Col>
+				  </Row>
+				</Grid>
+      </div>
     );
   }
 }
+
+
